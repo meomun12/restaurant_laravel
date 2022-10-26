89ae4eb7d6c32cbc595f5bf2317818ee73026595
unknown <quangdhph07309@fpt.edu.vn>
2022-10-26 10:00:13 +0700

    update cate

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Category;
 class CategoryController extends Controller
 {
     public function index(){
+        $category = Category::all();
+        return view('category.index');
+
+    }
+    public function create(){
+           return view('category.create');
+
+    }
+    public function stores(){
 
     }
 }
